61349a17a76474f5f5af2c7f2a239a60a66f0eeb
Peter Daily <peter.daily@yahoo.co.uk>
2016-06-17 15:01:45 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+require('./styles/main.css') // root stylesheeet - .css, .scss or .sass
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+ReactDOM.render(<App />, document.querySelector('.container'));
