606015b7fb17b043ecee2a43127413fcc612bb93
Peter Daily <peter.daily@yahoo.co.uk>
2016-06-19 18:49:47 +0100

    Add CV button

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,8 @@ export default () => {
   return (
     <header>
       <div className="nav">
-        <Button title="Contact me" url="#Get-In-Touch"/>
+        <Button title="CV" url="//drive.google.com/file/d/0B6imXc7m6sp6RGwzY1FudXJjXzg/view?usp=sharing" />
+        <Button title="Contact me" url="#Get-In-Touch" />
       </div>
     </header>
   )
