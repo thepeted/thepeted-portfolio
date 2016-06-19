8e109b6fcc080d9aa7544b0156afb61480122261
Peter Daily <peter.daily@yahoo.co.uk>
2016-06-19 21:42:55 +0100

    re-order showcases and fix typos

--- src/data/showcase-data.js
+++ src/data/showcase-data.js
@@ -2,21 +2,7 @@ import React from 'react';
 
 export default [
   {
-    description: 'The Grid is a fully functional multi-level 2D Dungeon Crawler with randomly generated maps and entities created using the React.js view library.  As well as creating the dungeon creation algorithms, the main challenge of this exercise was to effectively manage the state of the app.  To solve this challenge I chose to use Redux, an implementation of the Flux pattern, and redux-thunk middleware (to handle async actions).  The UI features a responsive viewport that tracks the player position and is aware of the edges of the game world.  Writing this app taught me lots about functional programming and creating organised code.  Give it a go - the game is challenging but winnable!',
-    hash: 'aNrdzP',
-    height: 620,
-    title: 'Dungeon Crawler',
-    fullWidth: true
-  },
-  {
-    description: 'If you&#8217;re not familiar with the Game of Life, it&#8217;s a cellular automation devised by British mathematician John Conway (you can read more about it on <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Wikipedia</a>).  This is an interactive version built with React.js and Redux with a simple UI that allows the user to add, remove or randomise the cells and see the effects on the evolution of the cells over time.  This was my first (and only!) demo to get picked on <a href="http://codepen.io">Codepen</a>.  I liked the color scheme and UI elements I created so much that I decided to use them on this website!',
-    hash: 'bpovxz',
-    height: 620,
-    title: 'Game of Life',
-    fullWidth: true
-  },
-  {
-    description: 'Play the ultimate game of champions - Noughts & Crosses, against a computer oponent.  What could be more fun?!  Certainly, I can confirm that writing the AI was a great deal more fun than playing the game.  Noughts & Crosses is a <a href="https://en.wikipedia.org/wiki/Zero-sum_game">zero sum game</a> and the <a href="https://en.wikipedia.org/wiki/Minimax">Minimax</a> algorithm seemed like a good choice for determining the AI gameplay rules.  It&#8217;s a recursive algorithm and so provided a good test of my understanding of recursion and my ability to translate an outline description of an algorithm in to JavaScript.  If you do manage to win a game, please contact me to claim a special prize.',
+    description: 'Play the ultimate game of champions - Noughts & Crosses - against a computer oponent.  What could be more fun?!  Certainly, I can confirm that writing the AI was a great deal more fun than playing the game.  Noughts & Crosses is a <a href="https://en.wikipedia.org/wiki/Zero-sum_game">zero sum game</a> and the <a href="https://en.wikipedia.org/wiki/Minimax">Minimax</a> algorithm seemed like a good choice for determining the AI gameplay rules.  It&#8217;s a recursive algorithm and so provided a good test of my understanding of recursion and my ability to translate an outline description of an algorithm in to JavaScript.  If you do manage to win a game, please contact me to claim a special prize.',
     hash: 'qOJVyK',
     height: 435,
     title: 'Noughts & Crosses',
@@ -37,7 +23,21 @@ export default [
     fullWidth: false
   },
   {
-    description: 'A fully functional implementaion of the venerable Simon game created using JQuery.  The user is challenged to recall and input a random sequence, increasing in length and speed up to 20 levels.  Also features a permadeath mode! I enjoyed the challenge of writing the CSS to build an attractive UI.  Trivia: This app prompted me to learn React.js so that I could manage application state more effectively and intuitively than using JQuery in my future projects.',
+    description: 'The Grid is a fully functional multi-level 2D Dungeon Crawler with randomly generated maps and entities created using the React.js view library.  As well as creating the dungeon creation algorithms, the main challenge of this exercise was to effectively manage the state of the app.  To solve this challenge I chose to use Redux, an implementation of the Flux pattern, and redux-thunk middleware (to handle async actions).  The UI features a responsive viewport that tracks the player position and is aware of the edges of the game world.  Writing this app taught me lots about functional programming and creating organised code.  Give it a go - the game is challenging but winnable!',
+    hash: 'aNrdzP',
+    height: 620,
+    title: 'Dungeon Crawler',
+    fullWidth: true
+  },
+  {
+    description: 'If you&#8217;re not familiar with the Game of Life, it&#8217;s a cellular automation devised by British mathematician John Conway (you can read more about it on <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Wikipedia</a>).  This is an interactive version built with React.js and Redux with a simple UI that allows the user to add, remove or randomise the cells and see the effects on the evolution of the cells over time.  This was my first (and only!) demo to get picked on <a href="http://codepen.io">Codepen</a>.  I liked the color scheme and UI elements I created so much that I decided to use them on this website!',
+    hash: 'bpovxz',
+    height: 620,
+    title: 'Game of Life',
+    fullWidth: true
+  },
+  {
+    description: 'A fully functional implementaion of the venerable Simon game created using JQuery.  The user is challenged to recall and input a random sequence, increasing in length and speed up to 20 levels.  Also features a permadeath mode! I enjoyed the challenge of writing the CSS to build an attractive UI.  Trivia: this app prompted me to learn React.js so that I could manage application state more effectively and intuitively than using JQuery in my future projects.',
     hash: 'ojVYoO',
     height: 550,
     title: 'Simon Game',
@@ -70,5 +70,5 @@ export default [
     height: 400,
     title: 'Twitch Status Viewer',
     fullWidth: false
-  },
+  }
 ]
