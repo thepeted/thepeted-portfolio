565819e8c9155c045f282a359fd71053e4245870
Peter Daily <peter.daily@yahoo.co.uk>
2017-09-17 22:52:53 +0100

    codepen embeds take theme; correct attribute naming

--- src/components/codepen.js
+++ src/components/codepen.js
@@ -5,15 +5,15 @@ export default ({hash, height}) => {
     <div className="codepen">
       <iframe
         id={`cp_embed_${hash}`}
-        src={`//codepen.io/thepeted/embed/${hash}?height=${height}&amp;theme-id=dark&amp;slug-hash=${hash}&amp;default-tab=result&amp;user=thepeted&amp;embed-version=2`}
+        src={`//codepen.io/thepeted/embed/${hash}?height=${height}&amp;theme-id=24278&amp;slug-hash=${hash}&amp;default-tab=result&amp;user=thepeted&amp;embed-version=2`}
         scrolling="no"
-        frameborder="0"
+        frameBorder="0"
         height={height}
-        allowtransparency="true"
-        allowfullscreen="true"
+        allowTransparency="true"
+        allowFullScreen="true"
         name="CodePen Embed"
         title="CodePen Embed"
-        class="cp_embed_iframe"
+        className="cp_embed_iframe"
         style={{width: '100%', overflow: 'hidden'}}></iframe>
     </div>
   )
