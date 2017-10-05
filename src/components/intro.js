4662ff416563d4ce964b1584b46a03bbd8c2a709
Peter Daily <peter.daily@yahoo.co.uk>
2017-10-05 19:18:50 +0100

    remove UK from title

--- src/components/intro.js
+++ src/components/intro.js
@@ -5,7 +5,7 @@ import { MIN_HEIGHT, SCROLL_SPEED }from '../constants/constants'
 const Intro = ({ height }) => (
   <div className="content intro" style={{ height, minHeight: MIN_HEIGHT + 'px' }}>
     <div>
-      <h1>Hello! I'm Peter Daily, a web developer based in Manchester, UK.</h1>
+      <h1>Hello! I'm Peter Daily, a web developer based in Manchester.</h1>
       <p>I'm passionate about creating beautiful and functional user interfaces. I usually work with JavaScript, React and Sass but picking up something new isn't a problem. </p>
       <p>
         I like to be creative and work on fun projects in my spare time - here are some of my favourites. <span className='button' onClick={() => scrollToElement('#work', { duration: SCROLL_SPEED })}>Go</span>
