13a2136c4c82ca3c80df59f68c634cb415ce3e1b
Peter Daily <peter.daily@yahoo.co.uk>
2017-10-20 15:40:42 +0100

    show spinner while iframe loads

--- src/components/codepen.js
+++ src/components/codepen.js
@@ -1,23 +1,58 @@
-import React from "react"
+import React, { Component } from "react"
 import PropTypes from "prop-types"
+import { RotatingPlane } from "better-react-spinkit"
 
-const CodePen = ({ hash, height }) => (
-  <div className="codepen">
-    <iframe
-      id={`cp_embed_${hash}`}
-      src={`//codepen.io/thepeted/embed/${hash}?height=${height}&amp;theme-id=24278&amp;slug-hash=${hash}&amp;default-tab=result&amp;user=thepeted&amp;embed-version=2`}
-      scrolling="no"
-      frameBorder="0"
-      height={height}
-      allowTransparency="true"
-      allowFullScreen="true"
-      name="CodePen Embed"
-      title="CodePen Embed"
-      className="cp_embed_iframe"
-      style={{ width: "100%", overflow: "hidden" }}
-    />
-  </div>
-)
+class CodePen extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loaded: false
+    }
+    this.handleLoad = this.handleLoad.bind(this)
+  }
+
+  getLoader() {
+    const loaderHeight = 50
+    const iFrameHeight = this.props.height
+    const top = iFrameHeight / 2 - loaderHeight / 2
+    return (
+      <div style={{ position: "absolute", top, left: "50%" }}>
+        <RotatingPlane size={loaderHeight} color="#db4500" duration="1s" />
+      </div>
+    )
+  }
+
+  handleLoad() {
+    this.setState({ loaded: true })
+  }
+
+  render() {
+    const { hash, height } = this.props
+    return (
+      <div className="codepen" style={{ position: "relative" }}>
+        {!this.state.loaded && this.getLoader()}
+        <iframe
+          id={`cp_embed_${hash}`}
+          src={`//codepen.io/thepeted/embed/${hash}?height=${height}&amp;theme-id=24278&amp;slug-hash=${hash}&amp;default-tab=result&amp;user=thepeted&amp;embed-version=2`}
+          scrolling="no"
+          frameBorder="0"
+          height={height}
+          allowTransparency="true"
+          allowFullScreen="true"
+          name="CodePen Embed"
+          title="CodePen Embed"
+          className="cp_embed_iframe"
+          style={{
+            width: "100%",
+            overflow: "hidden",
+            opacity: `${this.state.loaded ? 1 : 0}`
+          }}
+          onLoad={this.handleLoad}
+        />
+      </div>
+    )
+  }
+}
 
 CodePen.propTypes = {
   hash: PropTypes.string.isRequired,
